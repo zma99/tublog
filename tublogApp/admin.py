9eef244521ad225fc93818db00b52b18c3901cc6
claudio <cmarangoni8@gmail.com>
2021-03-04 19:05:30 -0300

    installo Pillow, se creo base de datos usuario, posteo, contacto

--- tublogApp/admin.py
+++ tublogApp/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Usuario, Posteo, Contacto
 
 # Register your models here.
+
+admin.site.register(Usuario)
+admin.site.register(Posteo)
+admin.site.register(Contacto)
--- tublogApp/models.py
+++ tublogApp/models.py
@@ -1,3 +1,31 @@
 from django.db import models
 
-# Create your models here.
+class Usuario(models.Model):
+    usuario = models.CharField(max_length=15, primary_key=True, unique=True)
+    contrasenia = models.CharField (max_length=10)
+    email = models.EmailField(max_length=20, unique=True)
+    nombre = models.CharField(max_length=10)
+    apellido = models.CharField(max_length=15)
+    fecha_nacimiento = models.DateField(auto_now=False, auto_now_add=False)
+    direccion = models.CharField(max_length=50)
+    ciudad = models.CharField(max_length=15)
+    provincia = models.CharField(max_length=15)
+
+
+class Posteo(models.Model):
+    id = models.AutoField(primary_key=True)
+    nombre = models.CharField(max_length=10)
+    apellido = models.CharField(max_length=15)
+    titulo = models.CharField(max_length=20)
+    objeto_panel = models.Manager()
+    comentario = models.CharField(max_length=50)
+    imagen = models.ImageField(upload_to = 'photos')
+   
+
+
+class Contacto(models.Model):
+    id = models.AutoField(primary_key=True)
+    email = models.EmailField(max_length=20, unique=True)
+    nombre = models.CharField(max_length=10)
+    apellido = models.CharField(max_length=15)
+    mensaje = models.CharField(max_length=150)
